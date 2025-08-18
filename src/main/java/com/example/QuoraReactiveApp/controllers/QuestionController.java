750ab0d9130176b4d0644c1f5dff5d2957e9191c
abhinavrajhans <abhinav.rajhans1@gmail.com>
2025-08-18 19:37:04 +0530

    added some unimplemented method signature

--- src/main/java/com/example/QuoraReactiveApp/controllers/QuestionController.java
+++ src/main/java/com/example/QuoraReactiveApp/controllers/QuestionController.java
@@ -5,9 +5,8 @@ import com.example.QuoraReactiveApp.dto.QuestionRequestDTO;
 import com.example.QuoraReactiveApp.dto.QuestionResponseDTO;
 import com.example.QuoraReactiveApp.services.QuestionService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -22,5 +21,38 @@ public class QuestionController {
         return questionService.createQuestion(questionRequestDTO)
                 .doOnSuccess(response -> System.out.println("Question created successfully: " + response))
                 .doOnError(error -> System.out.println("Error creating question: " + error));
+    }
+
+    @GetMapping
+    public Mono<QuestionResponseDTO> getQuestionById(@RequestParam Long questionId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @GetMapping
+    public Flux<QuestionResponseDTO> getAllQuestions() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deleteQuestionById(@PathVariable String questionId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @GetMapping("/search")
+    public Flux<QuestionResponseDTO> searchQuestions(
+            @RequestParam String query,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "100") int size
+    ){
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+
+    @GetMapping("/tag/{tag}")
+    public Flux<QuestionResponseDTO> getQuestionsByTag(@PathVariable String tag,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "100") int size
+    ){
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
